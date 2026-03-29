28b2be948c22b990df30d6f68a6686e0e83132cd
khorum_keeper <139578299+violabs-josh@users.noreply.github.com>
2026-03-28 21:49:24 -0500

    Add @DefaultState annotation with DefaultStateType enum for common defaults (#7)

---
+++ src/main/kotlin/org/khorum/oss/konstellation/metaDsl/annotation/DefaultState.kt
@@ -0,0 +1,33 @@
+package org.khorum.oss.konstellation.metaDsl.annotation
+
+/**
+ * Specifies a predefined default state for a property in the generated DSL builder.
+ *
+ * Use this annotation for common defaults (empty string, zero, empty collections, booleans)
+ * instead of [@DefaultValue][DefaultValue] to avoid string-escaping issues and improve readability.
+ *
+ * ```kotlin
+ * @GeneratedDsl
+ * data class Config(
+ *     @DefaultState(DefaultStateType.EMPTY_STRING)
+ *     val name: String,
+ *
+ *     @DefaultState(DefaultStateType.ZERO_INT)
+ *     val retryCount: Int,
+ *
+ *     @DefaultState(DefaultStateType.EMPTY_LIST)
+ *     val tags: MutableList<String>,
+ *
+ *     @DefaultState(DefaultStateType.FALSE)
+ *     val enabled: Boolean
+ * )
+ * ```
+ *
+ * **Mutual exclusivity:** A property must not have both `@DefaultState` and `@DefaultValue`.
+ * The KSP processor will emit an error if both are present.
+ *
+ * @property type The predefined default state to apply. See [DefaultStateType] for available options.
+ */
+@Target(AnnotationTarget.PROPERTY)
+@Retention(AnnotationRetention.SOURCE)
+annotation class DefaultState(val type: DefaultStateType)
